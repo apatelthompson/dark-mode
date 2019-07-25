3606d1864a73244f7f8e6c3cc5122cd64ac274c3
avni <avni@hellorosey.com>
2019-07-25 22:24:07 +0200

    started on Local Storage function

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function useLocalStorage (key, initial value) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  })};
+
+
+
+export default useLocalStorage;

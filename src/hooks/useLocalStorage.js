24402f017c2bf2723e11d8ef270063e0870c4c1c
avni <avni@hellorosey.com>
2019-07-25 22:31:03 +0200

    completed creating custom localStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-function useLocalStorage (key, initial value) {
+function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = window.localStorage.getItem(key);
@@ -9,8 +9,20 @@ function useLocalStorage (key, initial value) {
       console.log(error);
       return initialValue;
     }
-  })};
+  });
 
+  const setValue = value => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  return [storedValue, setValue];
+}
 
 export default useLocalStorage;
